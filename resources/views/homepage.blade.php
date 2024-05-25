3b5b8666cf45c58b4e435eb84317edb29ac7d03b
rfdzan <hekodev.py@gmail.com>
2024-05-25 15:52:57 +0700

    basic page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.default')
+@section('content')
+<section class="profile-container">
+    <div class="profile">
+        <p class="section-title">Rifdi</p>
+        <p class="short-description">just a guy</p>
+    </div>
+</section>
+<section class="works-container">
+    <div class="works">
+        <p class="section-title">What has he built?</p>
+        <p class="works-description">quite a lot, actually.</p>
+    </div>
+    <div class="my-works-container">
+        <div class="my-works-1">column 1</div>
+        <div class="my-works-2">column 2</div>
+        <div class="my-works-3">column 3</div>
+    </div>
+</section>
+@endsection
